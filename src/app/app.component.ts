6d5055979a82b072fc1613ef792bd7663e053f5b
Brande George <george.brande@gmail.com>
2018-11-11 15:58:22 +0200

    form OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,12 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ProductsService } from './products.service';
-import {Subscription} from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  title = 'fitness-tracker';
-  productName = '';
+export class AppComponent  {
 
-  products = [];
-  private productsSubscription: Subscription;
-
-  constructor(private productsService: ProductsService) {
-  }
-
-  ngOnInit() {
-    this.products = this.productsService.getProducts();
-    this.productsSubscription = this.productsService.productsUpdated.subscribe(
-      () => {
-        this.products = this.productsService.getProducts();
-      }
-    );
-
-  }
-
-  onAddProduct(form) {
-    // this.products.push(this.productName);.
-    // console.log(form);
-    this.productsService.addProduct(form.value.productName);
-  }
-
-  onRemoveProduct(productName: string) {
-    this.products = this.products.filter(p => p !== productName);
-  }
-
-  ngOnDestroy() {
-    this.productsSubscription.unsubscribe();
+  constructor() {
   }
 }
