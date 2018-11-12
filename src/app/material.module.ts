f91228486a193334e841b8e9b64a06959b5c4ebf
Brande George <george.brande@gmail.com>
2018-11-12 20:36:29 +0200

    before angular firebase

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -15,7 +15,9 @@ import {
     MatSelectModule,
     MatProgressSpinnerModule,
     MatDialogModule,
-    MatTableModule
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule
 } from '@angular/material';
 
 @NgModule({
@@ -35,7 +37,10 @@ import {
         MatSelectModule,
         MatProgressSpinnerModule,
         MatDialogModule,
-        MatTableModule],
+        MatTableModule,
+        MatSortModule,
+        MatPaginatorModule
+    ],
 
     exports: [
         MatButtonModule,
@@ -53,7 +58,10 @@ import {
         MatSelectModule,
         MatProgressSpinnerModule,
         MatDialogModule,
-        MatTableModule]
+        MatTableModule,
+        MatSortModule,
+        MatPaginatorModule
+    ]
 })
 export class MaterialModule {
 
